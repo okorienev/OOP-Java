7a14fae446988cb33d386197e3e56185c2bbea20
Oleksandr Korienev <alexkorienev@gmail.com>
2018-04-16 23:50:59 +0300

    start of lab 6 - Collections
    Node class for LinkedList
    Iterator class (currently is only a structure stub)
    MyList<E> implementing List<E> (internal structure is linked list)
    currentrly implemented(poorly tested):
    - size()
    - isEmpty()
    - constructors from single element and without parameters
    - adding single element

---
+++ lab_6/src/List/MyIterator.java
@@ -0,0 +1,15 @@
+package List;
+
+import java.util.Iterator;
+
+public class MyIterator implements Iterator {
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Object next() {
+        return null;
+    }
+}
---
+++ lab_6/src/List/MyList.java
@@ -0,0 +1,155 @@
+package List;
+
+import java.util.*;
+
+public class MyList<E> implements List<E> {
+    private int size = 0;
+    private Node<E> head;
+    private Node<E> tail;
+
+    public MyList(E data) {
+        Node<E> tmp = new Node<>(data);
+        this.head = tmp;
+        this.tail = tmp;
+        size = 1;
+    }
+
+    public MyList(){
+        
+    }
+
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size == 0){
+            return true;
+        }else {
+            return true;
+        }
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public boolean add(E o){
+        if (o == null){
+            throw new NullPointerException();
+        }
+        Node<E> tmp = new Node<>(o);
+        if (head == null){
+            head = tmp;
+            tail = tmp;
+            size = 1;
+        }else {
+            tail.setNext(tmp);
+            tmp.setPrev(tail);
+            tail = tmp;
+            size ++;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, Object element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean retainAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection c) {
+        return false;
+    }
+
+    @Override
+    public Object[] toArray(Object[] a) {
+        return new Object[0];
+    }
+}
+
